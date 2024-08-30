34bf408f150c4272cbd98aff916c15902b45952a
beckermr <becker.mr@gmail.com>
2024-08-30 06:19:47 -0500

    feat: add rerender

---
+++ conda_forge_feedstock_ops/container_utils.py
@@ -0,0 +1,191 @@
+import json
+import logging
+import os
+import pprint
+import subprocess
+from typing import Callable, Iterable, Optional
+
+from ._version import __version__
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CONTAINER_TMPFS_SIZE_MB = 6000
+
+
+def get_default_container_name():
+    """Get the default container name for feedstock ops.
+
+    The image is stored at `ghcr.io/regro/conda-forge-feedstock-ops`.
+
+    If the environment variable `CF_FEEDSTOCK_OPS_CONTAINER_NAME` is set, then that name is used.
+
+    If the environment variable `CF_FEEDSTOCK_OPS_CONTAINER_TAG` is set, then that tag is pulled.
+    Otherwise, we pull the tag `__version__`.
+    """
+    cname = (
+        f"{os.environ.get('CF_FEEDSTOCK_OPS_CONTAINER_NAME', 'ghcr.io/regro/conda-forge-feedstock-ops')}"
+        + f":{os.environ.get('CF_FEEDSTOCK_OPS_CONTAINER_TAG', __version__)}"
+    )
+
+    return cname
+
+
+class ContainerRuntimeError(RuntimeError):
+    """An error raised when running a container fails."""
+
+    def __init__(self, *, error, name, cmd, returncode, traceback=None):
+        self.name = name
+        self.cmd = cmd
+        self.returncode = returncode
+        self.traceback = traceback
+        super().__init__(error)
+
+
+def get_default_container_run_args(
+    tmpfs_size_mb: int = DEFAULT_CONTAINER_TMPFS_SIZE_MB,
+):
+    """Get the default arguments for running a container.
+
+    Parameters
+    ----------
+    tmpfs_size_mb : int, optional
+        The size of the tmpfs in MB, by default 10.
+
+    Returns
+    -------
+    list
+        The command to run a container.
+    """
+    extra_env_vars = []
+
+    tmpfs_size_bytes = tmpfs_size_mb * 1000 * 1000
+    return (
+        [
+            "docker",
+            "run",
+            "-e",
+            "CF_FEEDSTOCK_OPS_IN_CONTAINER=true",
+        ]
+        + extra_env_vars
+        + [
+            "--security-opt=no-new-privileges",
+            "--read-only",
+            "--cap-drop=all",
+            "--mount",
+            f"type=tmpfs,destination=/tmp,tmpfs-mode=1777,tmpfs-size={tmpfs_size_bytes}",
+            "-m",
+            "6000m",
+            "--cpus",
+            "1",
+            "--ulimit",
+            "nofile=1024:1024",
+            "--ulimit",
+            "nproc=2048:2048",
+            "--rm",
+            "-i",
+        ]
+    )
+
+
+def run_container_operation(
+    name: str,
+    args: Iterable[str],
+    json_loads: Callable = json.loads,
+    tmpfs_size_mb: int = DEFAULT_CONTAINER_TMPFS_SIZE_MB,
+    input: Optional[str] = None,
+    mount_dir: Optional[str] = None,
+    mount_readonly: bool = True,
+):
+    """Run a feedstock operation in a container.
+
+    Parameters
+    ----------
+    name
+        The name of the operation.
+    args
+        The arguments to pass to the container.
+    json_loads
+        The function to use to load JSON to a string, by default `json.loads`.
+    tmpfs_size_mb
+        The size of the tmpfs in MB, by default 10.
+    input
+        The input to pass to the container, by default None.
+    mount_dir
+        The directory to mount to the container at `/cf_tick_dir`, by default None.
+    mount_readonly
+        Whether to mount the directory as read-only, by default True.
+
+    Returns
+    -------
+    data : dict-like
+        The result of the operation.
+    """
+    if mount_dir is not None:
+        mount_dir = os.path.abspath(mount_dir)
+        mnt_args = ["--mount", f"type=bind,source={mount_dir},destination=/cf_tick_dir"]
+        if mount_readonly:
+            mnt_args[-1] += ",readonly"
+    else:
+        mnt_args = []
+
+    log_level_str = str(logging.getLevelName(logger.getEffectiveLevel())).lower()
+    logger.debug("computed effective logging level: %s", log_level_str)
+
+    cmd = [
+        *get_default_container_run_args(tmpfs_size_mb=tmpfs_size_mb),
+        *mnt_args,
+        get_default_container_name(),
+        "conda-forge-feedstock-ops",
+        name,
+        *args,
+        "--log-level",
+        log_level_str,
+    ]
+    res = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        text=True,
+        input=input,
+    )
+    # we handle this ourselves to customize the error message
+    if res.returncode != 0:
+        raise ContainerRuntimeError(
+            error=f"Error running {name} in container - return code {res.returncode}:"
+            f"\ncmd: {pprint.pformat(cmd)}"
+            f"\noutput: {pprint.pformat(res.stdout)}",
+            name=name,
+            cmd=pprint.pformat(cmd),
+            returncode=res.returncode,
+        )
+
+    try:
+        ret = json_loads(res.stdout)
+    except json.JSONDecodeError:
+        raise ContainerRuntimeError(
+            error=f"Error running {name} in container - JSON could not parse stdout:"
+            f"\ncmd: {pprint.pformat(cmd)}"
+            f"\noutput: {pprint.pformat(res.stdout)}",
+            name=name,
+            cmd=pprint.pformat(cmd),
+            returncode=res.returncode,
+        )
+
+    if "error" in ret:
+        ret_str = (
+            ret["error"]
+            .split("(", maxsplit=1)[1]
+            .rsplit(")", maxsplit=1)[0]
+            .encode("raw_unicode_escape")
+            .decode("unicode_escape")
+        )
+        raise ContainerRuntimeError(
+            error=f"Error running {name} in container - error {ret['error'].split('(')[0]} raised:\n{ret_str}",
+            name=name,
+            cmd=pprint.pformat(cmd),
+            returncode=res.returncode,
+            traceback=ret["traceback"]
+            .encode("raw_unicode_escape")
+            .decode("unicode_escape"),
+        )
+
+    return ret["data"]
---
+++ conda_forge_feedstock_ops/json.py
@@ -0,0 +1,80 @@
+
+import logging
+from collections.abc import Callable
+from typing import (
+    IO,
+    Any,
+    Set,
+)
+
+import rapidjson as json
+
+logger = logging.getLogger(__name__)
+
+
+def default(obj: Any) -> Any:
+    """For custom object serialization."""
+    if isinstance(obj, Set):
+        return {"__set__": True, "elements": sorted(obj)}
+    raise TypeError(repr(obj) + " is not JSON serializable")
+
+
+def object_hook(dct: dict) -> Any:
+    """For custom object deserialization."""
+    if "__set__" in dct:
+        return set(dct["elements"])
+    return dct
+
+
+def dumps(
+    obj: Any,
+    sort_keys: bool = True,
+    separators: Any = (",", ":"),
+    default: "Callable[[Any], Any]" = default,
+    **kwargs: Any,
+) -> str:
+    """Returns a JSON string from a Python object."""
+    return json.dumps(
+        obj,
+        sort_keys=sort_keys,
+        # separators=separators,
+        default=default,
+        indent=1,
+        **kwargs,
+    )
+
+
+def dump(
+    obj: Any,
+    fp: IO[str],
+    sort_keys: bool = True,
+    separators: Any = (",", ":"),
+    default: "Callable[[Any], Any]" = default,
+    **kwargs: Any,
+) -> None:
+    """Returns a JSON string from a Python object."""
+    return json.dump(
+        obj,
+        fp,
+        sort_keys=sort_keys,
+        # separators=separators,
+        default=default,
+        indent=1,
+        **kwargs,
+    )
+
+
+def loads(
+    s: str, object_hook: "Callable[[dict], Any]" = object_hook, **kwargs: Any
+) -> dict:
+    """Loads a string as JSON, with appropriate object hooks"""
+    return json.loads(s, object_hook=object_hook, **kwargs)
+
+
+def load(
+    fp: IO[str],
+    object_hook: "Callable[[dict], Any]" = object_hook,
+    **kwargs: Any,
+) -> dict:
+    """Loads a file object as JSON, with appropriate object hooks."""
+    return json.load(fp, object_hook=object_hook, **kwargs)
