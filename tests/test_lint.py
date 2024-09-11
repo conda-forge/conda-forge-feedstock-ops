fd6b5fdeb893317e64e0154ef016e953615af69d
beckermr <becker.mr@gmail.com>
2024-09-11 06:25:28 -0500

    test: add test w/ more than one recipe

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -6,20 +6,24 @@ from conda_forge_feedstock_ops.lint import lint
 
 
 @skipif_no_github_token
-def test_lint_llvmdev_local():
-    feedstock_dir = os.path.join(os.path.dirname(__file__), "data", "llvmdev-feedstock")
+def test_lint_local():
+    feedstock_dir = os.path.join(os.path.dirname(__file__), "data")
     lints, hints = lint(
         feedstock_dir,
         use_container=False,
     )
     assert len(hints) + len(lints) > 0
+    all_keys = set(lints.keys()) | set(hints.keys())
+    assert all_keys == {"llvmdev-feedstock/recipe/meta.yaml", "ngmix-blah/recipe/meta.yaml"}
 
 
 @skipif_no_containers
-def test_parse_package_and_feedstock_names_llvmdev_container(use_containers):
-    feedstock_dir = os.path.join(os.path.dirname(__file__), "data", "llvmdev-feedstock")
+def test_lint_container(use_containers):
+    feedstock_dir = os.path.join(os.path.dirname(__file__), "data")
     lints, hints = lint(
         feedstock_dir,
         use_container=False,
     )
     assert len(hints) + len(lints) > 0
+    all_keys = set(lints.keys()) | set(hints.keys())
+    assert all_keys == {"llvmdev-feedstock/recipe/meta.yaml", "ngmix-blah/recipe/meta.yaml"}
