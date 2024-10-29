9f44f9af587ae6f6e446932f2b0e64cad0e7f5b4
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-10-29 10:10:20 -0500

    fix: bug force add

--- conda_forge_feedstock_ops/__main__.py
+++ conda_forge_feedstock_ops/__main__.py
@@ -234,7 +234,7 @@ def _rerender_feedstock(*, timeout):
 
             _execute_git_cmds_and_report(
                 cmds=[
-                    ["git", "add", "."],
+                    ["git", "add", "-f", "."],
                     ["git", "commit", "-am", msg],
                 ],
                 cwd=fs_dir,
