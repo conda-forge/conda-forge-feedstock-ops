7c47fdb9bdec39e6035160691c71f52b07abf2f8
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-01-26 14:42:03 -0600

    Apply suggestion from @beckermr

--- conda_forge_feedstock_ops/rerender.py
+++ conda_forge_feedstock_ops/rerender.py
@@ -110,17 +110,9 @@ def rerender_containerized(feedstock_dir, exclusive_config_file=None, timeout=No
                 tmpdir, os.path.basename(str(exclusive_config_file))
             )
             shutil.copyfile(str(exclusive_config_file), tmp_exclusive_config_file)
-            args += ["--exclusive-config-file", tmp_exclusive_config_file]
+            args += ["--exclusive-config-file", os.path.basename(tmp_exclusive_config_file)]
             logger.debug(
                 "copied host exclusive config file: %s", tmp_exclusive_config_file
-            )
-            print(
-                "CBC:",
-                exclusive_config_file,
-                tmp_exclusive_config_file,
-                os.path.exists(exclusive_config_file),
-                os.path.exists(tmp_exclusive_config_file),
-                flush=True,
             )
 
         chmod_plus_rwX(tmpdir, recursive=True)
