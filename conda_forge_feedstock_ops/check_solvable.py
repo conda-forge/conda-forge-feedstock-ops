2585d36394dbd6bf6010387391e82cbd533b49ba
beckermr <becker.mr@gmail.com>
2026-04-07 05:28:41 -0500

    fix: see exact variants

--- conda_forge_feedstock_ops/check_solvable.py
+++ conda_forge_feedstock_ops/check_solvable.py
@@ -394,6 +394,11 @@ def _is_recipe_solvable_on_platform(
 
         timeout_timer.raise_for_timeout()
 
+        import io
+        cbc_io = io.StringIO()
+        parser.dump(cbc, cbc_io)
+        print_debug("rendered variants:\n" + cbc_io.getvalue())
+
         if os.path.exists(os.path.join(recipe_dir, "recipe.yaml")):
             # this is a rattler-build recipe so we can invoke rattler-build with
             # the new `cbc`.
