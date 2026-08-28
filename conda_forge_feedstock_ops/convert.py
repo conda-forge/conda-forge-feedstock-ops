a152133a58a4fff2e79abc86a1fcac83d4347fcf
beckermr <becker.mr@gmail.com>
2026-08-28 06:08:00 -0500

    fix: recipe conversion error false positive for 1 warning

--- conda_forge_feedstock_ops/convert.py
+++ conda_forge_feedstock_ops/convert.py
@@ -144,7 +144,12 @@ def _post_process_returncode_and_stderr(returncode, stderr):
         if "Could not patch unrecognized license" in line:
             continue
 
-        if "errors and" in line and "warning" in line and line.startswith("0"):
+        if (
+            " error" in line
+            and " and " in line
+            and " warning" in line
+            and line.startswith("0")
+        ):
             continue
 
         new_lines.append(line.strip())
