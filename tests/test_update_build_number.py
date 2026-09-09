4829b6b8293d606757a6c9ff917b2ed9623e367e
beckermr <becker.mr@gmail.com>
2026-09-09 13:44:58 -0500

    test: make tests more specific and fix them

--- tests/test_update_build_number.py
+++ tests/test_update_build_number.py
@@ -48,30 +48,33 @@ def test_update_build_number_v0_function(meta_yaml, new_meta_yaml, tmp_path):
 
 
 @pytest.mark.parametrize(
-    "meta_yaml,new_meta_yaml",
+    "meta_yaml,new_meta_yaml,did_update",
     [
-        ("build:\n  number: 2", "build:\n  number: 0\n"),
+        ("build:\n  number: 2", "build:\n  number: 0\n", True),
         (
             "outputs:\n  - build:\n      number: 2\n",
             "outputs:\n  - build:\n      number: 0\n",
+            True,
         ),
-        ("context:\n  build_number: 2\n", "context:\n  build_number: 0\n"),
-        ("context:\n  build: 2\n", "context:\n  build: 0\n"),
+        ("context:\n  build_number: 2\n", "context:\n  build_number: 0\n", True),
+        ("context:\n  build: 2\n", "context:\n  build: 0\n", True),
         (
-            "context:\n  build_num: 2\nbuild\n  number: ${{ build_num }}",
-            "context:\n  build_num: 0\nbuild\n  number: ${{ build_num }}",
+            "context:\n  build_num: 2\nbuild:\n  number: ${{ build_num }}\n",
+            "context:\n  build_num: 0\nbuild:\n  number: ${{ build_num }}\n",
+            True,
         ),
         (
-            "context:\n  build_nu: 2\nbuild\n  number: ${{ build_num }}",
-            "context:\n  build_nu: 2\nbuild\n  number: ${{ build_num }}",
+            "context:\n  build_nu: 2\nbuild:\n  number: ${{ build_num }}\n",
+            "context:\n  build_nu: 2\nbuild:\n  number: ${{ build_num }}\n",
+            False,
         ),
     ],
 )
-def test_update_build_number_v1(meta_yaml, new_meta_yaml, tmp_path):
+def test_update_build_number_v1(meta_yaml, new_meta_yaml, did_update, tmp_path):
     filename = tmp_path / "recipe.yaml"
     filename.write_text(meta_yaml)
-    did_update = update_build_number(filename, 0)
-    assert did_update
+    actual_did_update = update_build_number(filename, 0)
+    assert did_update is actual_did_update
     out_meta_yaml = filename.read_text()
     assert out_meta_yaml == new_meta_yaml
 
